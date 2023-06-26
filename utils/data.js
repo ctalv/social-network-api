18dfdaabf0da17010eb5fe1e279de6cb7316c42d
ctalv <122413805+ctalv@users.noreply.github.com>
2023-06-26 14:32:05 -0500

    added data.js and seed.js pseudocode

---
+++ utils/data.js
@@ -0,0 +1,3 @@
+// seed data in array format
+
+// export data
---
+++ utils/seed.js
@@ -0,0 +1,9 @@
+// require connection.js, models, and data
+
+// start the seeding runtime timer
+
+// create a connection to mongodb (async)
+
+// await all the data to be seeded in the db
+
+// log out
